c79533c5c5bf0c95dae70a08b8901574f7f653f6
Jure Mraović <mraovic.jure@gmail.com>
2022-04-26 17:33:39 +0200

    Update init.js

--- init.js
+++ init.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   
+    // The following will initialise nice select on all native select fields with class .orderby  
+  
     $('select.orderby').each(function() {
         $(this).niceSelect({
-            cssClass: 'custom-nice-select-style', // Class name without dot
-            selectedText: 'Sort By', // If anything used, it will be fixed and label won't change, otherwise updating with selected option text
+            cssClass: 'custom-nice-select-style', // Class name for custom styling
+            selectedText: 'Sort By', // If anything used, it will be fixed label, otherwise updating with selected option text
             customIcon: '<i class="fal fa-sort-alpha-down"></i>', // Fontawesome icon, custom image, whatever, default is an arrow
         });
     });
